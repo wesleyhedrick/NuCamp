4f28f5e30e6b1d0dfd53044e9385ce3b4fe1f848
GSWes <wesley.hedrick@gmail.com>
2021-06-16 21:47:21 -0400

    stuff

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 $(function () {
     $(".carousel").carousel({ interval: 2000 });
-    $('#carouselButton').click(function () {
+    $('#carouselButton').on('click', function () {
         if ($("#carouselButton").children("i").hasClass("fa-pause")) {
             $(".carousel").carousel("pause")
             $("#carouselButton").children("i").removeClass("fa-pause")
@@ -11,4 +11,12 @@ $(function () {
             $("#carouselButton").children("i").addClass("fa-pause")
         }
     });
+    $('#reserveButton').on('click', () => {
+        $('#reserveModal').modal('show')
+    })
+
+    $('#loginButton').on('click', () => {
+        $('#loginModal').modal('show')
+    })
+
 })
